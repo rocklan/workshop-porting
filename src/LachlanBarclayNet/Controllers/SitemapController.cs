591055498eb049003fb432a425fa8b74e8c5f049
Lachlan Barclay <lachlan.barclay@mercer.com>
2021-01-25 11:08:47 +1100

    moved viewmodel and viewmodel creator logic to standard project

--- src/LachlanBarclayNet/Controllers/SitemapController.cs
+++ src/LachlanBarclayNet/Controllers/SitemapController.cs
@@ -1,11 +1,8 @@
 ﻿using LachlanBarclayNet.DAO;
 using LachlanBarclayNet.ViewModel;
+
 using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
 using System.Web.Mvc;
 
 namespace LachlanBarclayNet.Controllers
@@ -17,27 +14,14 @@ namespace LachlanBarclayNet.Controllers
         public ActionResult Index()
         {
             PostDAO postDAO = new PostDAO();
-            var posts = postDAO.GetAllLivePosts();
 
-            SitemapIndexViewModel ViewModel = new SitemapIndexViewModel
-            {
-                Urls = posts.Select(x => new SitemapUrl { Url = x.FullUrl }).ToList()
-            };
+            List<LachlanBarclayNet.DAO.Standard.Post> posts = postDAO.GetAllLivePosts();
 
-            AddUrl(ViewModel, "contact");
+            SitemapIndexViewModel ViewModel = SitemapIndexViewModelFactory.Create(posts);
                 
             Response.ContentType = "application/xml";
 
             return View(ViewModel);
-            
         }
-
-        private void AddUrl(SitemapIndexViewModel ViewModel, string uri)
-        {
-            ViewModel.Urls.Add(new SitemapUrl { Url = ConfigurationManager.AppSettings["domain"] + "/" + uri });
-        }
-
     }
-
-    
 }
---
+++ src/LachlanBarclayNetDb/viewmodels/SitemapIndexViewModelFactory.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+
+namespace LachlanBarclayNet.ViewModel
+{
+    public class SitemapIndexViewModelFactory
+    {
+        public static SitemapIndexViewModel Create(IEnumerable<LachlanBarclayNet.DAO.Standard.Post> Posts)
+        {
+            SitemapIndexViewModel ViewModel = new SitemapIndexViewModel
+            {
+                Urls = Posts.Select(x => new SitemapUrl { Url = x.FullUrl }).ToList()
+            };
+
+            AddUrl(ViewModel, "contact");
+
+            return ViewModel;
+        }
+
+        private static void AddUrl(SitemapIndexViewModel ViewModel, string uri)
+        {
+            ViewModel.Urls.Add(new SitemapUrl { Url = ConfigurationManager.AppSettings["domain"] + "/" + uri });
+        }
+    }
+}
