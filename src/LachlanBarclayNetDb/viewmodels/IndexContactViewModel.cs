bcf346295ba1cffc5bea32a77a71aea01906c14a
Lachlan Barclay <lachlan.barclay@mercer.com>
2021-01-27 09:45:07 +1100

    moved viewmodels into standard project

---
+++ src/LachlanBarclayNetDb/viewmodels/IndexContactViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace LachlanBarclayNet.ViewModel
+{
+    public class IndexContactViewModel
+    {
+        public List<YearPost> Years { get; set; }
+        public string Category { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required,RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Please enter a valid e-mail adress")]
+        public string Email { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+
+        public bool EmailSent { get; set; }
+
+        [Required]
+        public string RecaptchaToken { get; set; }
+    }
+}
